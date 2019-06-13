a5704b7de4a256e0b90eb46d751b79ff253f5171
Ryan Kelln <ryan.kelln@gmail.com>
2019-06-12 21:16:43 -0400

    Fixes and hacks for coding chaos poetry

--- demo.py
+++ demo.py
@@ -114,13 +114,14 @@ class Hand(object):
         stroke_colors = stroke_colors or ['black']*len(lines)
         stroke_widths = stroke_widths or [2]*len(lines)
 
-        line_height = 60
-        view_width = 1000
+        line_height = 55
+        view_width = 0
+        width_padding = 40
         view_height = line_height*(len(strokes) + 1)
 
         dwg = svgwrite.Drawing(filename=filename)
-        dwg.viewbox(width=view_width, height=view_height)
-        dwg.add(dwg.rect(insert=(0, 0), size=(view_width, view_height), fill=background_color))
+
+        #dwg.add(dwg.rect(insert=(0, 0), size=(view_width, view_height), fill=background_color))
 
         initial_coord = np.array([0, -(3*line_height / 4)])
         for offsets, line, color, width in zip(strokes, lines, stroke_colors, stroke_widths):
@@ -129,6 +130,8 @@ class Hand(object):
                 initial_coord[1] -= line_height
                 continue
 
+            line_class = "linestart"
+
             offsets[:, :2] *= 1.5
             strokes = drawing.offsets_to_coords(offsets)
             strokes = drawing.denoise(strokes)
@@ -136,19 +139,34 @@ class Hand(object):
 
             strokes[:, 1] *= -1
             strokes[:, :2] -= strokes[:, :2].min() + initial_coord
-            strokes[:, 0] += (view_width - strokes[:, 0].max()) / 2
+            #strokes[:, 0] += (view_width - strokes[:, 0].max()) / 2  # center text
 
             prev_eos = 1.0
             p = "M{},{} ".format(0, 0)
             for x, y, eos in zip(*strokes.T):
-                p += '{}{},{} '.format('M' if prev_eos == 1.0 else 'L', x, y)
+                if x  > view_width:
+                    view_width = x
+                # p += '{}{},{} '.format('M' if prev_eos == 1.0 else 'L', x, y)
+                # create a new path each time we move
+                if prev_eos == 1.0:
+                    path = svgwrite.path.Path(p)
+                    path = path.stroke(color=color, width=width, linecap='round').fill("none")
+                    dwg.add(path)
+                    p = '{}{},{} '.format('M', x, y)
+                else:
+                    p += '{}{},{} '.format('L', x, y)
                 prev_eos = eos
             path = svgwrite.path.Path(p)
             path = path.stroke(color=color, width=width, linecap='round').fill("none")
+            if line_class != None:
+                    path.update({'class_': line_class})
+                    line_class = None
             dwg.add(path)
 
             initial_coord[1] -= line_height
 
+        view_width = int(view_width + width_padding)
+        dwg.viewbox(width=view_width, height=view_height)
         dwg.save()
 
 
