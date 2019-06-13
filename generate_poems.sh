ae0efb8314cb4bb114b78ef8be462d95d433fd54
Ryan Kelln <ryan.kelln@gmail.com>
2019-06-12 21:17:12 -0400

    Add poem files

---
+++ generate_poems.sh
@@ -0,0 +1,23 @@
+# remember to activate the conda environment that the libaries were installed in:
+# $ conda activate magenta_gpu
+python run.py --infile poems/poem_suzy_1.txt --outfile poems/poem_suzy_1.svg --color white --style 0 --width 2 --bias 0.6
+python run.py --infile poems/poem_suzy_2.txt --outfile poems/poem_suzy_2.svg --color white --style 0 --width 2 --bias 0.6
+python run.py --infile poems/poem_suzy_3.txt --outfile poems/poem_suzy_3.svg --color white --style 0 --width 2 --bias 0.6
+python run.py --infile poems/poem_suzy_3b.txt --outfile poems/poem_suzy_3b.svg --color white --style 0 --width 2 --bias 0.6
+python run.py --infile poems/poem_suzy_4.txt --outfile poems/poem_suzy_4.svg --color white --style 0 --width 2 --bias 0.6
+
+python run.py --infile poems/poem_nebyu_1.txt  --outfile poems/poem_nebyu_01.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_2.txt  --outfile poems/poem_nebyu_02.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_3.txt  --outfile poems/poem_nebyu_03.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_4.txt  --outfile poems/poem_nebyu_04.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_5.txt  --outfile poems/poem_nebyu_05.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_5b.txt --outfile poems/poem_nebyu_05b.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_6.txt  --outfile poems/poem_nebyu_06.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_7.txt  --outfile poems/poem_nebyu_07.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_8.txt  --outfile poems/poem_nebyu_08.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_9.txt  --outfile poems/poem_nebyu_09.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_10.txt --outfile poems/poem_nebyu_10.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_11.txt --outfile poems/poem_nebyu_11.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_12.txt --outfile poems/poem_nebyu_12.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_13.txt --outfile poems/poem_nebyu_13.svg --color white --style 1 --width 2 --bias 0.6
+python run.py --infile poems/poem_nebyu_14.txt --outfile poems/poem_nebyu_14.svg --color white --style 1 --width 2 --bias 0.6
